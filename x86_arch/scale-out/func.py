76734b61c26df92d1f4a343a072535de6c4768b4
Venugopal Naik <46160810+naikvenu@users.noreply.github.com>
2023-10-23 18:50:54 +1100

    initial commit

--- x86_arch/scale-out/func.py
+++ x86_arch/scale-out/func.py
@@ -1,4 +1,11 @@
+## Copyright (c) 2022 Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
+#Disclaimer:
+#This script is provided for experimental purposes only and should not be used in production. 
+# It is provided to assist your development or administration efforts and provided “AS IS” and is NOT supported by Oracle Corporation. 
+#The script has been tested in a test environment and appears to work as intended. You should always run new scripts on a test environment and validate and modify the same as per your requirements before using on your application environment.
+ 
 import io
 import os
 from fdk import response
@@ -10,6 +17,7 @@ from oci.core import ComputeManagementClientCompositeOperations
 # Instance Pool ID
 instance_pool_id = os.getenv('INSTANCE_POOL_ID', 'not-configured')
 instance_pool_max_size = os.getenv('INSTANCE_POOL_MAX_SIZE', 'not-configured')
+instance_pool_increment_size = os.getenv('INSTANCE_POOL_INCREMENT_SIZE', 'not-configured')
 
 def handler(ctx, data: io.BytesIO = None):
     """
@@ -24,28 +32,38 @@ def handler(ctx, data: io.BytesIO = None):
         log_body = data.getvalue()
 
         #print('context: ', dir(ctx), 'Body: ',len(log_body), 'body ops:', dir(log_body))
-        print("Contents of the body for reference: ", log_body)
+        print("Contents of the Body for Reference: ", log_body)
+        
+        # Check if the Configuration values are valid
         
         if instance_pool_max_size == 'not-configured':
-            print('INSTANCE_POOL_MAX_SIZE is not configured. Nothing to do.')
+            print('INSTANCE_POOL_MAX_SIZE is not configured or its a invalid non integer value. please specify a value between 1 to 100.')
             return response.Response(ctx, response_data={'result': 'No action required'},
                                      headers={"Content-Type": "application/json"})
             
+        if isinstance(instance_pool_max_size, int) and isinstance(instance_pool_increment_size):
+            print('Valid integer values for INSTANCE_POOL_MAX_SIZE and INSTANCE_POOL_INCREMENT_SIZE supplied ..')
+        else:
+            print('Invalid integer values for INSTANCE_POOL_MAX_SIZE and INSTANCE_POOL_INCREMENT_SIZE supplied ..')
+            return response.Response(ctx, response_data={'result': 'No action required'},
+                                     headers={"Content-Type": "application/json"})
+        
+        if (0 < instance_pool_max_size <= 100):
+            print('INSTANCE_POOL_MAX_SIZE is within the acceptable hard limit of 1-100')
+        else:
+            print('INSTANCE_POOL_MAX_SIZE is not within the acceptable hard limit of 1-100')
+            return response.Response(ctx, response_data={'result': 'No action required'},
+                                     headers={"Content-Type": "application/json"})
+             
         signer = oci.auth.signers.get_resource_principals_signer()
         compute_management_client = ComputeManagementClient(config={}, signer=signer)
         composite_client = ComputeManagementClientCompositeOperations(compute_management_client)
        
         instance_pool = compute_management_client.get_instance_pool(instance_pool_id).data
 
-        print('Instance pool size: ',instance_pool.size,'Instance pool Name: ',\
-            instance_pool.display_name, 'instance pool id: ', instance_pool_id)
+        print('Instance Pool Size: ',instance_pool.size,'Instance Pool Name: ',\
+            instance_pool.display_name, 'Instance Pool Id: ', instance_pool_id)
         
-        # Check if instance_pool size is at its maximum
-        if int(instance_pool_max_size) <= 1:
-            resp = 'Instance pool Max size is invalid. Pool Max size should be greater than 1.'
-            print(resp)
-            return response.Response(ctx, response_data={'result': resp + ' Not Scaling'},
-                                     headers={"Content-Type": "application/json"})
         
         # Check if instance_pool size is at its maximum
         if instance_pool.size == int(instance_pool_max_size):
@@ -58,8 +76,16 @@ def handler(ctx, data: io.BytesIO = None):
             print('Instance pool is in state: ', {instance_pool.lifecycle_state},'. No further action.')
             return response.Response(ctx, response_data={'result': 'Instance Pool not in RUNNING state, Not Scaling'},
                                      headers={"Content-Type": "application/json"})
-
-        increased_pool_size = instance_pool.size + 1
+        
+        if instance_pool_increment_size == 'not-configured' or (not isinstance(instance_pool_increment_size, int)):
+            increased_pool_size = instance_pool.size + 1
+        elif (instance_pool.size + instance_pool_increment_size) > instance_pool_max_size:
+            print('Instance pool size: ',instance_pool.size,'+', instance_pool_increment_size,' is greater than max pool limit ',instance_pool_max_size)
+            print('Just incrementing the pool size by 1')
+            increased_pool_size = instance_pool.size + 1
+        else:
+            increased_pool_size = instance_pool.size + instance_pool_increment_size
+            
         update_details = UpdateInstancePoolDetails(size=increased_pool_size)
         composite_client.update_instance_pool_and_wait_for_state(instance_pool.id, update_details,
                                                                  wait_for_states=["SCALING"])
@@ -70,7 +96,3 @@ def handler(ctx, data: io.BytesIO = None):
     except Exception as err:
         print("Error in handler: {}".format(str(err)))
         raise err
-
-
-
-
